2a0c0ccdc78af0de502d9d2a8943aba3c31db170
advitiya0201 <f20210979@pilani.bits-pilani.ac.in>
2025-03-01 18:55:35 +0530

    ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
-
+    for(String it: args) {
+      System.out.println(it);
+    }
      final String command = args[0];
 
      switch (command) {
